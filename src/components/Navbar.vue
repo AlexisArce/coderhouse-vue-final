9c2eb9233db952b56260c70b27609468b8766490
aarce <Arce.Alexis@hotmail.com>
2021-12-20 17:23:24 -0300

    Add logo in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,22 @@
 <template lang="html">
-  <v-toolbar color="black" dark fixed>
-    <router-link to="/">
-      <v-toolbar-title>e-commerce</v-toolbar-title>
-    </router-link>
-    <v-spacer></v-spacer>
-    <router-link to="/cart">
-      <v-btn icon>
-        <v-icon>mdi-cart</v-icon>
-      </v-btn>
-    </router-link>
-  </v-toolbar>
+  <v-app-bar color="black" app>
+    <v-toolbar color="black" dark fixed>
+      <router-link to="/">
+        <v-img
+          src="../assets/logo_By_Wix_2.jpg"
+          max-height="100"
+          max-width="200"
+          contain
+        ></v-img>
+      </router-link>
+      <v-spacer></v-spacer>
+      <router-link to="/cart">
+        <v-btn icon>
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+      </router-link>
+    </v-toolbar>
+  </v-app-bar>
 </template>
 
 <script lang="js">
