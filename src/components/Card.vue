5917ff6dabda97790197fd3320a4091aae165c7a
aarce <Arce.Alexis@hotmail.com>
2022-01-26 18:04:13 -0300

    Add Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+  <v-card class="pa-2" outlined>
+    <v-img
+      :src="product.image"
+      :lazy-src="product.image"
+      class="white--text align-end"
+      gradient="to bottom, rgba(0,0,0,.1),
+            rgba(0,0,0,.5)"
+      max-height="250"
+      max-width="500"
+    >
+      <v-card-title v-text="product.title"></v-card-title>
+    </v-img>
+
+    <v-card-actions>
+      <v-chip class="ma-2" large dark>$ {{ product.price }} </v-chip>
+      <el-tag v-if="product.isVeggie" type="success">Veggie</el-tag>
+      <v-spacer></v-spacer>
+      <el-tooltip
+        class="item"
+        effect="dark"
+        content="ver detalles"
+        placement="left"
+      >
+        <el-button
+          icon="el-icon-search"
+          circle
+          @click="viewItemDetails(product)"
+        ></el-button>
+      </el-tooltip>
+      <el-button type="danger" @click="addProductToCart(product)"
+        >Agregar</el-button
+      >
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'card',
+    props: ["product"],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss"></style>
--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -10,40 +10,7 @@
         :key="product.id"
         class="col-md-3"
       >
-        <v-card class="pa-2" outlined>
-          <v-img
-            :src="product.image"
-            :lazy-src="product.image"
-            class="white--text align-end"
-            gradient="to bottom, rgba(0,0,0,.1),
-            rgba(0,0,0,.5)"
-            max-height="250"
-            max-width="500"
-          >
-            <v-card-title v-text="product.title"></v-card-title>
-          </v-img>
-
-          <v-card-actions>
-            <v-chip class="ma-2" large dark>$ {{ product.price }} </v-chip>
-            <el-tag v-if="product.isVeggie" type="success">Veggie</el-tag>
-            <v-spacer></v-spacer>
-            <el-tooltip
-              class="item"
-              effect="dark"
-              content="ver detalles"
-              placement="left"
-            >
-              <el-button
-                icon="el-icon-search"
-                circle
-                @click="viewItemDetails(product)"
-              ></el-button>
-            </el-tooltip>
-            <el-button type="danger" @click="addProductToCart(product)"
-              >Agregar</el-button
-            >
-          </v-card-actions>
-        </v-card>
+        <card :product="product"></card>
       </v-col>
     </v-row>
   </section>
@@ -51,10 +18,11 @@
 
 <script>
 import Spinner from "./Spinner";
+import Card from "./Card";
 
 export default {
   name: "item-list",
-  components: { Spinner },
+  components: { Spinner, Card },
   props: [],
   data: () => ({}),
 
