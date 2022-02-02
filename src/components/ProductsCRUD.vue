f18825ac1aef4ccb2ffcc43908ece8fece2b3000
aarce <Arce.Alexis@hotmail.com>
2022-02-02 01:39:03 -0300

    Fix ProductsCRUD component

--- src/components/ProductsCRUD.vue
+++ src/components/ProductsCRUD.vue
@@ -1,85 +1,109 @@
 <template>
   <section class="products-crud">
-    <v-data-table
-      :headers="headers"
-      :items="products"
-      :items-per-page="10"
-      :loading="loadingProducts"
-      :search="search"
-      class="elevation-1"
-      loading-text="Cargando la información..."
-    >
-      <template v-slot:item.isVeggie="{ item }">
-        <v-chip :color="chipColor(item)" dark> {{ chipText(item) }} </v-chip>
-      </template>
-
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Buscar"
-            single-line
-            hide-details
-          ></v-text-field>
-          <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="1000px">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-                Nuevo producto
-              </v-btn>
+    <v-container fluid>
+      <v-row justify="space-around">
+        <v-col class="text-center" cols="12" sm="6">
+          <h1>Productos</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="12" md="12" lg="12">
+          <v-data-table
+            :headers="headers"
+            :items="products"
+            :items-per-page="10"
+            :loading="loadingProducts"
+            :search="search"
+            class="elevation-1"
+            loading-text="Cargando la información..."
+          >
+            <template v-slot:item.isVeggie="{ item }">
+              <v-chip :color="chipColor(item)" dark>
+                {{ chipText(item) }}
+              </v-chip>
             </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <products-form :edited-item="editedItem" ref="productForm" />
-                </v-container>
-              </v-card-text>
-              <v-card-actions>
+
+            <template v-slot:top>
+              <v-toolbar flat>
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Buscar"
+                  single-line
+                  hide-details
+                ></v-text-field>
                 <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">
-                  Cancelar
-                </v-btn>
-                <v-btn color="blue darken-1" text @click="save">
-                  Guardar
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="600px">
-            <v-card>
-              <v-card-title class="text-h5"
-                >¿Está seguro de eliminar el registro?</v-card-title
-              >
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">
-                  Cancelar
-                </v-btn>
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="deleteItemConfirm(editedItem)"
-                >
-                  Sí
-                </v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize"> Reset </v-btn>
-      </template>
-    </v-data-table>
+                <v-dialog v-model="dialog" max-width="1000px">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      color="primary"
+                      dark
+                      class="mb-2"
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      Nuevo producto
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="text-h5">{{ formTitle }}</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <products-form
+                          :edited-item="editedItem"
+                          ref="productForm"
+                        />
+                      </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue darken-1" text @click="close">
+                        Cancelar
+                      </v-btn>
+                      <v-btn color="blue darken-1" text @click="save">
+                        Guardar
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+                <v-dialog v-model="dialogDelete" max-width="600px">
+                  <v-card>
+                    <v-card-title class="text-h5"
+                      >¿Está seguro de eliminar el registro?</v-card-title
+                    >
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue darken-1" text @click="closeDelete">
+                        Cancelar
+                      </v-btn>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="deleteItemConfirm(editedItem)"
+                      >
+                        Sí
+                      </v-btn>
+                      <v-spacer></v-spacer>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-toolbar>
+            </template>
+            <template v-slot:item.actions="{ item }">
+              <v-icon small class="mr-2" @click="editItem(item)">
+                mdi-pencil
+              </v-icon>
+              <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+            </template>
+            <template v-slot:no-data>
+              <v-btn color="primary" @click="initialize"> Reset </v-btn>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
   </section>
 </template>
 
@@ -232,4 +256,8 @@ const defaultItem = () => {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+  font-size: 1.5em !important;
+}
+</style>
